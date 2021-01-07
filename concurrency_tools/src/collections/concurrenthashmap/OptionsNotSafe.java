4a3b0969ed4e31a6d2524d5a70e2f36ef14b235e
wysss <1243876958@qq.com>
2021-01-08 06:12:55 +0800

    ConcurrentHashMap实践

---
+++ concurrency_tools/src/collections/concurrenthashmap/OptionsNotSafe.java
@@ -0,0 +1,29 @@
+package collections.concurrenthashmap;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class OptionsNotSafe implements Runnable{
+    
+    private static ConcurrentHashMap<String,Integer> scores=
+            new ConcurrentHashMap<String,Integer>();
+
+    public static void main(String[] args) throws InterruptedException {
+        scores.put("小明",0);
+        Thread t1=new Thread(new OptionsNotSafe());
+        Thread t2=new Thread(new OptionsNotSafe());
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(scores);
+    }
+
+    @Override
+    public void run(){
+        for(int i=0;i<1000;i++){
+            Integer score=scores.get("小明");
+            Integer newScore=score+1;
+            scores.put("小明",newScore);
+        }
+    }
+}
