c1040b9850a55f86b6f4fd9f1a67b985c54ed813
wysss <1243876958@qq.com>
2021-01-09 06:21:41 +0800

    future实践

---
+++ concurrency_tools/src/future/OneFuture.java
@@ -0,0 +1,21 @@
+package future;
+
+import java.util.Random;
+import java.util.concurrent.*;
+
+public class OneFuture {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService service= Executors.newFixedThreadPool(10);
+        Future<Integer> future=service.submit(new CallableTask());
+        System.out.println(future.get());
+
+    }
+
+    static class CallableTask implements Callable<Integer>{
+        @Override
+        public Integer call() throws Exception{
+            Thread.sleep(3000);
+            return new Random().nextInt();
+        }
+    }
+}
