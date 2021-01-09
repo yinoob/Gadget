709bc71cf87801820bdbe029de0db21485e71c42
wysss <1243876958@qq.com>
2021-01-09 13:03:31 +0800

    缓存设置了有效期，到期自动失效

--- concurrency_tools/src/cache/Cache4.java
+++ concurrency_tools/src/cache/Cache4.java
@@ -9,28 +9,28 @@ import java.util.concurrent.*;
 //出于安全性考虑，缓存需要设置有效期，到期自动失效
 public class Cache4<A,V> implements Computable<A,V> {
 
-    private final Map<A, Future<V>> cache=new ConcurrentHashMap<>();
+    private final Map<A, Future<V>> cache = new ConcurrentHashMap<>();
 
-    private final Computable<A,V> c;
+    private final Computable<A, V> c;
 
-    public Cache4(Computable<A,V> c){
-        this.c=c;
+    public Cache4(Computable<A, V> c) {
+        this.c = c;
     }
 
     @Override
-    public V compute(A arg) throws Exception{
-        Future<V> f=cache.get(arg);
-        if(f==null){
-            Callable<V> callable=new Callable<V>() {
+    public V compute(A arg) throws Exception {
+        Future<V> f = cache.get(arg);
+        if (f == null) {
+            Callable<V> callable = new Callable<V>() {
                 @Override
                 public V call() throws Exception {
                     return c.compute(arg);
                 }
             };
-            FutureTask<V> ft=new FutureTask<>(callable);
-            f=cache.putIfAbsent(arg,ft);
-            if(f==null){
-                f=ft;
+            FutureTask<V> ft = new FutureTask<>(callable);
+            f = cache.putIfAbsent(arg, ft);
+            if (f == null) {
+                f = ft;
                 System.out.println("从FutureTask调用了计算函数");
                 ft.run();
             }
@@ -38,26 +38,31 @@ public class Cache4<A,V> implements Computable<A,V> {
         return f.get();
     }
 
-    public final static ScheduledExecutorService executor=
+    public V computeRandomExpire(A arg) throws Exception {
+        long randomExpire=(long)(Math.random()*10000);
+        return compute(arg,randomExpire);
+    }
+
+    public final static ScheduledExecutorService executor =
             Executors.newScheduledThreadPool(5);
 
-    public V compute(A arg,long expire) throws Exception {
-        if(expire>0){
+    public V compute(A arg, long expire) throws Exception {
+        if (expire > 0) {
             executor.schedule(new Runnable() {
                 @Override
                 public void run() {
                     expire(arg);
                 }
-            },expire,TimeUnit.MILLISECONDS);
+            }, expire, TimeUnit.MILLISECONDS);
 
         }
         return compute(arg);
     }
 
-    public synchronized void expire(A key){
-        Future<V> future=cache.get(key);
-        if(future!=null){
-            if(!future.isDone()){
+    public synchronized void expire(A key) {
+        Future<V> future = cache.get(key);
+        if (future != null) {
+            if (!future.isDone()) {
                 System.out.println("Future任务被取消");
                 future.cancel(true);
             }
@@ -66,14 +71,14 @@ public class Cache4<A,V> implements Computable<A,V> {
         }
     }
 
-    public static void main(String[] args)throws Exception{
-        Cache4<String,Integer> expensiveComputer=new Cache4<>(new ExpensiveFunction());
+    public static void main(String[] args) throws Exception {
+        Cache4<String, Integer> expensiveComputer = new Cache4<>(new ExpensiveFunction());
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    Integer result=expensiveComputer.compute("25",5000L);
-                    System.out.println("第一次的计算结果"+result);
+                    Integer result = expensiveComputer.compute("25", 5000L);
+                    System.out.println("第一次的计算结果" + result);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -83,8 +88,8 @@ public class Cache4<A,V> implements Computable<A,V> {
             @Override
             public void run() {
                 try {
-                    Integer result=expensiveComputer.compute("25");
-                    System.out.println("第二次的计算结果"+result);
+                    Integer result = expensiveComputer.compute("25");
+                    System.out.println("第二次的计算结果" + result);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -94,8 +99,8 @@ public class Cache4<A,V> implements Computable<A,V> {
             @Override
             public void run() {
                 try {
-                    Integer result=expensiveComputer.compute("24");
-                    System.out.println("第三次的计算结果"+result);
+                    Integer result = expensiveComputer.compute("24");
+                    System.out.println("第三次的计算结果" + result);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -103,10 +108,11 @@ public class Cache4<A,V> implements Computable<A,V> {
         }).start();
 
         Thread.sleep(6000L);
-        Integer result=expensiveComputer.compute("25");
-        System.out.println("主线程的计算结果"+result);
+        Integer result = expensiveComputer.compute("25");
+        System.out.println("主线程的计算结果" + result);
     }
-
-
-
 }
+
+
+
+
